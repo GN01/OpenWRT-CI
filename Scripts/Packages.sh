842abefc783c871306d45d27508ef1525320e37c
gongweijin <gongweijin@banggood.com>
2025-12-29 13:47:36 +0800

    修复v2ray-geodata版本冲突: 删除官方冲突包, 使用kenzok8统一版本

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -147,9 +147,10 @@ function git_sparse_clone() {
 
 #删除官方的默认插件（保留dae，使用官方版本）
 rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,bypass*}
-rm -rf ../feeds/packages/net/{v2ray-geodata}
+#删除冲突的geodata包（v2ray-geoip和v2ray-geosite版本不匹配）
+rm -rf ../feeds/packages/net/{v2ray-geodata,v2ray-geoip,v2ray-geosite,xray-geodata}
 
 #从kenzok8/small-package稀疏克隆插件
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-adguardhome luci-app-oaf
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-adguardhome luci-app-oaf v2ray-geodata
 
 
