21b16baaff525edc206445aed29850283dda18d7
gongweijin <gongweijin@banggood.com>
2026-01-06 11:33:59 +0800

    禁用argon主题自定义修改

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -70,16 +70,16 @@ if [ -f "$HP_INIT" ]; then
 	cd $PKG_PATH && echo "homeproxy init.d has been patched!"
 fi
 
-#修改argon主题字体和颜色
-if [ -d *"luci-theme-argon"* ]; then
-	echo " "
-
-	cd ./luci-theme-argon/
-
-	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
-
-	cd $PKG_PATH && echo "theme-argon has been fixed!"
-fi
+#修改argon主题字体和颜色（已禁用，使用默认配置）
+#if [ -d *"luci-theme-argon"* ]; then
+#	echo " "
+#
+#	cd ./luci-theme-argon/
+#
+#	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
+#
+#	cd $PKG_PATH && echo "theme-argon has been fixed!"
+#fi
 
 #修改qca-nss-drv启动顺序
 NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
